c88dcd37c9b469ee1c3132808f6db208dfe70803
Juha Komulainen <juha.komulainen@evident.fi>
2017-12-12 22:36:31 +0200

    Day 9: Stream Processing

---
+++ src/test/kotlin/komu/adventofcode/StreamProcessingTest.kt
@@ -0,0 +1,29 @@
+package komu.adventofcode
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class StreamProcessingTest {
+
+    @Test
+    fun `example data`() {
+        assertEquals(1, streamScore("{}"))
+        assertEquals(6, streamScore("{{{}}}"))
+        assertEquals(5, streamScore("{{},{}}"))
+        assertEquals(16, streamScore("{{{},{},{{}}}}"))
+        assertEquals(1, streamScore("{<a>,<a>,<a>,<a>}"))
+        assertEquals(9, streamScore("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
+        assertEquals(9, streamScore("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
+        assertEquals(3, streamScore("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
+    }
+
+    @Test
+    fun `real data`() {
+        assertEquals(17537, streamScore(readTestInput("input/StreamProcessing.txt")))
+    }
+
+    @Test
+    fun `real data 2`() {
+        assertEquals(7539, streamGarbage(readTestInput("input/StreamProcessing.txt")))
+    }
+}
