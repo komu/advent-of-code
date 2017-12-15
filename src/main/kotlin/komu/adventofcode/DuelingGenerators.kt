31a7929eca1225cd77ba180f1a0d89c1c19464f9
Juha Komulainen <juha.komulainen@evident.fi>
2017-12-15 09:36:44 +0200

    Day 15: Dueling Generators

---
+++ src/main/kotlin/komu/adventofcode/DuelingGenerators.kt
@@ -0,0 +1,22 @@
+package komu.adventofcode
+
+import kotlin.coroutines.experimental.buildSequence
+
+fun duelingGenerators(startA: Int, startB: Int, rounds: Int, multiplesOfA: Int = 1, multiplesOfB: Int = 1) =
+    generator(factor = 16807, start = startA, multiplier = multiplesOfA)
+            .zip(generator(factor = 48271, start = startB, multiplier = multiplesOfB))
+            .take(rounds)
+            .count { (a, b) -> a.lowBits == b.lowBits }
+
+private fun generator(factor: Int, start: Int, multiplier: Int = 1) = buildSequence {
+    var state = start.toLong()
+
+    while (true) {
+        state = (state * factor) % 2147483647
+
+        if (state.toInt() % multiplier == 0)
+            yield(state.toInt())
+    }
+}
+
+private val Int.lowBits get() = this and 65535
