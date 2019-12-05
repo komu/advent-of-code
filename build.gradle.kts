992797dcaacf5653b76ffc925de37b9748d040e0
Juha Komulainen <juha.komulainen@evident.fi>
2019-12-05 22:10:38 +0200

    Update JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,14 @@
-import org.gradle.kotlin.dsl.kotlin
-import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.60"
-    id("org.junit.platform.gradle.plugin") version "1.0.0"
 }
 
 repositories {
     jcenter()
 }
 
-val junitVersion = "5.0.0"
+val junitVersion = "5.5.2"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
@@ -19,7 +16,11 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
