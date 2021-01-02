0c3356943c5c38eef023441cbf48c7351b9cff38
Juha Komulainen <juha.komulainen@evident.fi>
2021-01-02 02:22:38 +0200

    Day 15: Rambunctious Recitation

---
+++ src/main/kotlin/aoc2020/RambunctiousRecitation.kt
@@ -0,0 +1,27 @@
+package aoc2020
+
+fun rambunctiousRecitation1(startingNumbers: List<Int>) =
+    rambunctiousRecitation(startingNumbers, 2020)
+
+fun rambunctiousRecitation2(startingNumbers: List<Int>) =
+    rambunctiousRecitation(startingNumbers, 30_000_000)
+
+private fun rambunctiousRecitation(startingNumbers: List<Int>, count: Int): Int {
+    val lastSpoken = IntArray(count) { -1 }
+
+    for ((turn, number) in startingNumbers.withIndex())
+        lastSpoken[number] = turn
+
+    var next = -1
+    var last = -1
+
+    for (turn in startingNumbers.size until count) {
+        val number = if (next == -1) 0 else (turn - 1) - next
+
+        next = lastSpoken[number]
+        lastSpoken[number] = turn
+        last = number
+    }
+
+    return last
+}
---
+++ src/test/kotlin/aoc2020/RambunctiousRecitationTest.kt
@@ -0,0 +1,39 @@
+package aoc2020
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class RambunctiousRecitationTest {
+
+    @Test
+    fun `example 1`() {
+        assertEquals(436, rambunctiousRecitation1(listOf(0, 3, 6)))
+        assertEquals(1, rambunctiousRecitation1(listOf(1, 3, 2)))
+        assertEquals(10, rambunctiousRecitation1(listOf(2, 1, 3)))
+        assertEquals(27, rambunctiousRecitation1(listOf(1, 2, 3)))
+        assertEquals(78, rambunctiousRecitation1(listOf(2, 3, 1)))
+        assertEquals(438, rambunctiousRecitation1(listOf(3, 2, 1)))
+        assertEquals(1836, rambunctiousRecitation1(listOf(3, 1, 2)))
+    }
+
+    @Test
+    fun `example 2`() {
+        assertEquals(175594, rambunctiousRecitation2(listOf(0, 3, 6)))
+        assertEquals(2578, rambunctiousRecitation2(listOf(1, 3, 2)))
+        assertEquals(3544142, rambunctiousRecitation2(listOf(2, 1, 3)))
+        assertEquals(261214, rambunctiousRecitation2(listOf(1, 2, 3)))
+        assertEquals(6895259, rambunctiousRecitation2(listOf(2, 3, 1)))
+        assertEquals(18, rambunctiousRecitation2(listOf(3, 2, 1)))
+        assertEquals(362, rambunctiousRecitation2(listOf(3, 1, 2)))
+    }
+
+    @Test
+    fun `part 1`() {
+        assertEquals(959, rambunctiousRecitation1(listOf(18, 11, 9, 0, 5, 1)))
+    }
+
+    @Test
+    fun `part 2`() {
+        assertEquals(116590, rambunctiousRecitation2(listOf(18, 11, 9, 0, 5, 1)))
+    }
+}
